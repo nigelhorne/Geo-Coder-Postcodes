ea9304959400f4b2bcefe20713799a82378310df
Nigel Horne <njh@bandsman.co.uk>
2017-10-09 09:51:13 -0400

    Initial Draft

---
+++ lib/Geo/Coder/Postcodes.pm
@@ -0,0 +1,182 @@
+package Geo::Coder::Postcodes;
+
+use strict;
+use warnings;
+
+use Carp;
+use Encode;
+use JSON;
+use HTTP::Request;
+use LWP::UserAgent;
+use LWP::Protocol::https;
+use URI;
+
+=head1 NAME
+
+Geo::Coder::Postcodes - Provides a geocoding functionality using https://postcodes.io.
+Note that this only works on towns and cities, it doesn't understand addresses or counties.
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+      use Geo::Coder::Postcodes;
+
+      my $geocoder = Geo::Coder::Postcodes->new();
+      my $location = $geocoder->geocode(location => 'Margate');
+
+=head1 DESCRIPTION
+
+Geo::Coder::Postcodes provides an interface to postcodes.io,
+a free geocode database covering the towns in the UK.
+
+=head1 METHODS
+
+=head2 new
+
+    $geocoder = Geo::Coder::Postcodes->new();
+    my $ua = LWP::UserAgent->new();
+    $ua->env_proxy(1);
+    $geocoder = Geo::Coder::Postcodes->new(ua => $ua);
+
+=cut
+
+sub new {
+	my($class, %param) = @_;
+
+	my $ua = delete $param{ua} || LWP::UserAgent->new(agent => __PACKAGE__ . "/$VERSION");
+	if(!defined($param{'host'})) {
+		$ua->ssl_opts(verify_hostname => 0);	# Yuck
+	}
+	my $host = delete $param{host} || 'postcodes.io';
+
+	return bless { ua => $ua, host => $host }, $class;
+}
+
+=head2 geocode
+
+    $location = $geocoder->geocode(location => $location);
+
+    print 'Latitude: ', $location->{'latitude'}, "\n";
+    print 'Longitude: ', $location->{'logitude'}, "\n";
+
+=cut
+
+sub geocode {
+	my $self = shift;
+
+	my %param;
+	if (@_ % 2 == 0) {
+		%param = @_;
+	} else {
+		$param{location} = shift;
+	}
+
+	my $location = $param{location}
+		or Carp::croak("Usage: geocode(location => \$location)");
+
+	if($location =~ /,/) {
+		Carp::croak('Postcodes.io only supports towns, not full addresses');
+	}
+
+	if (Encode::is_utf8($location)) {
+		$location = Encode::encode_utf8($location);
+	}
+
+	my $uri = URI->new("https://$self->{host}/places/");
+	$location =~ s/\s/+/g;
+	my %query_parameters = ('q' => $location);
+	$uri->query_form(%query_parameters);
+	my $url = $uri->as_string;
+
+	my $res = $self->{ua}->get($url);
+
+	if ($res->is_error) {
+		Carp::croak("postcodes.io API returned error: on $url " . $res->status_line());
+	}
+
+	my $json = JSON->new->utf8;
+
+	# TODO: wantarray
+	my $rc = $json->decode($res->content);
+	my @results = @{$rc->{result}};
+	return $results[0];
+}
+
+=head2 ua
+
+Accessor method to get and set UserAgent object used internally. You
+can call I<env_proxy> for example, to get the proxy information from
+environment variables:
+
+    $geocoder->ua()->env_proxy(1);
+
+You can also set your own User-Agent object:
+
+    use LWP::UserAgent::Throttled;
+    $geocoder->ua(LWP::UserAgent::Throttled->new());
+
+=cut
+
+sub ua {
+	my $self = shift;
+	if (@_) {
+		$self->{ua} = shift;
+	}
+	$self->{ua};
+}
+
+=head2 reverse_geocode
+
+    $location = $geocoder->reverse_geocode(latlng => '37.778907,-122.39732');
+
+Similar to geocode except it expects a latitude/longitude parameter.
+
+=cut
+
+sub reverse_geocode {
+	my $self = shift;
+
+	my %param;
+	if (@_ % 2 == 0) {
+		%param = @_;
+	} else {
+		$param{latlng} = shift;
+	}
+
+	my $latlng = $param{latlng}
+		or Carp::croak("Usage: reverse_geocode(latlng => \$latlng)");
+
+	return $self->geocode(location => $latlng, reverse => 1);
+};
+
+=head1 AUTHOR
+
+Nigel Horne <njh@bandsman.co.uk>
+
+Based on L<Geo::Coder::Coder::Googleplaces>.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+Lots of thanks to the folks at postcodes.io.
+
+=head1 SEE ALSO
+
+L<Geo::Coder::GooglePlaces>, L<HTML::GoogleMaps::V3>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2017 Nigel Horne.
+
+This program is released under the following licence: GPL2
+
+=cut
+
+1;
---
+++ t/uk.t
@@ -0,0 +1,54 @@
+#!perl -w
+
+use warnings;
+use strict;
+use Test::Most tests => 6;
+
+BEGIN {
+	use_ok('Geo::Coder::Postcodes');
+}
+
+UK: {
+	SKIP: {
+		skip 'Test requires Internet access', 2 unless(-e 't/online.enabled');
+
+		require Test::LWP::UserAgent;
+		Test::LWP::UserAgent->import();
+
+		require Test::Carp;
+		Test::Carp->import();;
+
+		eval {
+			require Test::Number::Delta;
+
+			Test::Number::Delta->import();
+		};
+
+		if($@) {
+			diag('Test::Number::Delta not installed - skipping tests');
+			skip 'Test::Number::Delta not installed', 2;
+		}
+
+		my $geocoder = new_ok('Geo::Coder::Postcodes');
+
+		my $location = $geocoder->geocode('Ramsgate');
+		delta_within($location->{latitude}, 51.33, 1e-2);
+		delta_within($location->{longitude}, 1.42, 1e-2);
+
+		eval {
+			does_carp_that_matches(sub { 
+				$location = $geocoder->geocode('Ramsgate, Kent, England');
+			}, qr/^postcodes.io only supports towns/);
+		};
+
+		my $ua = new_ok('Test::LWP::UserAgent');
+		$ua->map_response('postcodes.io', new_ok('HTTP::Response' => [ '500' ]));
+
+		$geocoder->ua($ua);
+		eval {
+			does_carp_that_matches(sub { 
+				$location = $geocoder->geocode('Sheffield');
+			}, qr/^postcodes.io API returned error: 500/);
+		};
+	}
+}
